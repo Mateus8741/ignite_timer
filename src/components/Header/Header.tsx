bc4934ec4c30262dba5b0edbb1439ff0d5d5fa09
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-12-02 19:20:27 -0300

    Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+
+import Logo from '../../assets/Logo.svg'
+import { Timer, Scroll } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="" />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
