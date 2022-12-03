3d25c1214985bb09f850b6021ced490f12346bfc
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-12-03 12:48:35 -0300

    Home Finished

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaulLayout'
 import { History } from './pages/History'
-import { Home } from './pages/Home'
+import { Home } from './pages/home'
 
 export function Router() {
   return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,19 @@ import { HeaderContainer } from './styles'
 
 import Logo from '../../assets/Logo.svg'
 import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={Logo} alt="" />
       <nav>
-        <a href="">
+        <NavLink to="/" title="Timer">
           <Timer size={24} />
-        </a>
-        <a href="">
+        </NavLink>
+        <NavLink to="/history" title="History">
           <Scroll size={24} />
-        </a>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
